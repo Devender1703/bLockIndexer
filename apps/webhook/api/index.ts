5d241eaba6d82da12c3d8fa7715696e17635734c
Devender <devender@Devenders-Air.domain.name>
2025-03-22 01:29:08 +0530

    Added Webhook handler

--- apps/webhook/api/index.ts
+++ apps/webhook/api/index.ts
@@ -3,6 +3,10 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.post("/webhook", (req, res) => {
+    res.send(`data from request ${req.body}`)
+});
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
