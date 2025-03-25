949d816577fcec461fc51fb8cf42fd8cae5a4be8
Devender <devender@Devenders-Air.domain.name>
2025-03-25 23:13:20 +0530

    Added middleware

--- apps/webhook/api/index.ts
+++ apps/webhook/api/index.ts
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     console.log("Check the logs")
     res.send("Express on Vercel")
 });
 
 app.post("/webhook", (req, res) => {
-    console.log(`check logs ${req}`)
-    res.send(`data from request ${req.body}`)
+    console.log(`check logs ${req.body}`)
+    res.send(`data from request ${JSON.stringify(req.body)}`)
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
