c8d9a60f00f3c9eef9cb7909fdb4f90cb9fb7ad3
Devender <devender@Devenders-Air.domain.name>
2025-03-26 09:14:30 +0530

    Added auth token

--- apps/webhook/api/index.ts
+++ apps/webhook/api/index.ts
@@ -8,15 +8,20 @@ app.get("/", (req, res) => {
     res.send("Express on Vercel")
 });
 
-app.post("/webhook", async (req, res) => {
-    console.log(`check logs ${req.body}`)
+app.post("/webhook", (req, res) => {
+    console.log(`check logs ${JSON.stringify(req.body)}`)
 
-    await fetch("s1251143.eu-nbg-2.betterstackdata.com", {
+    fetch("https://s1251143.eu-nbg-2.betterstackdata.com", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: { "Content-Type": "application/json",
+            "Authorization": `Bearer JmySQo4VdjHDyLcvT1soWhB6`
+        },
         body: JSON.stringify(req.body)
+    }).then((val) => {
+        res.send(`data from request ${JSON.stringify(req.body)}`)
+    }).catch((err) => {
+        res.send(`error from api ${err}`)
     });
-    res.send(`data from request ${JSON.stringify(req.body)}`)
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
